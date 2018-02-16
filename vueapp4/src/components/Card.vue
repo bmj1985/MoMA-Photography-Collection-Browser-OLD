4f08b34a90ca357f3f75ea0cd6bfda280aa5ddc4
Brandon Johnson <bmj1985@gmail.com>
2018-02-16 15:35:22 -0700

    initial commit

--- vueapp4/src/components/Card.vue
+++ vueapp4/src/components/Card.vue
@@ -4,12 +4,12 @@
   <div class="card border-primary mb-3" style="max-width: 20rem;"
   v-for="artwork in artworks.slice(0,10)" :key="artwork.ObjectID" >
   <div class="card-header">
-    {{Medium}}
+    {{artwork.Medium}}
     </div>
   <div class="card-body text-primary">
-    <input type="radio" id="one" :value="artwork.Artist[0]" v-model="artistData" v-on:click="nameToUrl(artistData)">
+    <input type="radio" id="one" :value="artwork.Artist[0]" v-model="artistData">
 <label for="one">Click here to get similar artist.</label>
-    <h4 class="card-title" v-on:click="nameToUrl(artistData)">
+    <h4 class="card-title">
       {{artwork.Artist[0]}}
       </h4>
     <img class="image" :src="artwork.ThumbnailURL" alt="">
@@ -31,38 +31,14 @@ import AttributeList from './AttributeList';
 export default {
   name: 'Card',
   components: { AttributeList },
-  props: ['artworks', 'Medium'],
+  props: ['artworks'],
   data() {
     return {
       infoToggle: true,
       artsyUrl: '',
-      token: '',
-      artistData: '',
-      defaultDiacriticsRemovalMap: []
+      token: localStorage.getItem('token'),
+      artistData: ''
     };
-  },
-  mounted () {
-    getResource()
-  },
-  methods: {
-    getResource() {
-      console.log('Hello World');
-      console.log('2nd line');
-      this.artsyUrl = `https://api.artsy.net/api/search?q=${
-        this.artistData
-      }&type=artist`;
-      console.log(this.artsyUrl);
-      fetch(this.artsyUrl, {
-        method: 'GET',
-        headers: new Headers({
-          'X-Xapp-Token': localStorage.getItem('token')
-        })
-      })
-        .then(response => response.json())
-        .then(response => {
-          console.log(response._embedded.results[0]._links.self.href);
-        });
-    }
   }
 };
 </script>
