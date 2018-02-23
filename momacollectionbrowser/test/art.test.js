c6fc04c1a0875d5af93b99e0d3e563ab7f536f0e
Brandon Johnson <bmj1985@gmail.com>
2018-02-22 17:11:19 -0700

    I think things are working in tests

--- momacollectionbrowser/test/art.test.js
+++ momacollectionbrowser/test/art.test.js
@@ -2,6 +2,7 @@
 // const test = require('ava');
 const artworks = require('../static/momaartworks.json');
 let departmentHeads = require('../static/momadepartmentheads.json');
+let photoCurators = [];
 const moment = require('moment');
 moment().format();
 
@@ -31,19 +32,14 @@ function formatDeptHeadData() {
 }
 
 function filterDepartmentHeads() {
-  return departmentHeads
-    .filter(head => {
-      return head.DepartmentFullName === 'Department of Photography';
-    })
-    .filter(head => {
-      return (
-        moment(artworkDate).isBefore(head.PositionEndYear) &&
-        moment(artworkDate).isAfter(head.PositionBeginYear)
-      );
-    });
+  let photoCurators = departmentHeads.filter(head => {
+    return head.DepartmentFullName === 'Department of Photography';
+  });
+  return photoCurators;
 }
 
 function getSpecificCurator() {
+  let photoHead = filterDepartmentHeads();
   let artworkDate = '';
   const photograph = artworks.filter((artwork, index) => {
     if (artwork.Department === 'Photography' && artwork.ThumbnailURL != null) {
@@ -53,13 +49,12 @@ function getSpecificCurator() {
   photograph.forEach(photo => {
     let artworkDate = photo.DateAcquired;
     new Date(artworkDate);
-    return departmentHeads.filter(head => {
+    console.log(photoHead);
+    return photoHead.filter(head => {
       moment(artworkDate).isBefore(head.PositionEndYear) &&
         moment(artworkDate).isAfter(head.PositionBeginYear);
     });
   });
-  // console.log(departmentHeads.slice(0, 10));
 }
 
-filterDepartmentHeads();
 getSpecificCurator();
